cb259fd4eec199486a19eef41da7ecee84942e88
m0xbf <m0xbf00@gmail.com>
2025-05-23 23:12:07 +0800

    update:0.1.1

--- src/feia/submodule.nim
+++ src/feia/submodule.nim
@@ -11,12 +11,12 @@ import mummy, mummy/routers
 proc indexHandler(request: Request) =
   var headers: HttpHeaders
   headers["Content-Type"] = "text/plain"
-  request.respond(200, headers, "Hello, World!")
+  request.respond(200, headers, "Hello, Feia!")
 
 proc runWeb*() =
   var router: Router
   router.get("/", indexHandler)
 
   let server = newServer(router)
-  echo "Serving on http://localhost:8080"
-  server.serve(Port(8080))
+  echo "Serving on http://localhost:10300"
+  server.serve(Port(10300))
